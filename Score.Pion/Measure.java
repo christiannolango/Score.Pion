4c09a61e4af52ef99f2cf18b4990e4b140df535e
Christian Nolan Go <chrisitannolango@gmail.com>
2017-02-25 17:15:28 -0700

    This class sets up the measures for the sheet music

--- Score.Pion/Measure.java
+++ Score.Pion/Measure.java
@@ -2,14 +2,27 @@
  * Created by Christian on 2/25/2017.
  */
 public class Measure {
-    private double keySignature = 4.00;
+    private static double keySignature = 4.00;
     private double beatsleft = 0.0; //beatsleft tells the program how many beats required to have a full measure
 
 
-    private double halfNote = 2.0;
-    private double quarterNote = 1.0;
-    private double eighthNote = 0.50;
-    private double sixteenthNote = 0.25;
+    private final double halfNote = 2.0;
+    private final double quarterNote = 1.0;
+    private final double eighthNote = 0.50;
+    private final double sixteenthNote = 0.25;
 
 
+    public Measure(int x) {
+        switch (x) {
+            case 1: keySignature = 4.00 - halfNote;
+                break;
+            case 2: keySignature = 4.00 - quarterNote;
+                break;
+            case 3: keySignature = 4.00 - eighthNote;
+                break;
+            case 4: keySignature = 4.00 - sixteenthNote;
+
+        }
+
+    }
 }
